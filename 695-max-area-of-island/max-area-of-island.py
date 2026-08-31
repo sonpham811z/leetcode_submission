c3e65ab88e03fec5d1120b7689ceff679e6926bf
Son Pham <142706433+sonpham811z@users.noreply.github.com>
2026-08-31 22:58:41 +0700

    Time: 23 ms (40.68%) | Memory: 20.9 MB (27.65%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -4,7 +4,6 @@ class Solution:
         cols = len(grid[0])
 
         max_area = 0
-        island=0
         tmp = 0
 
         def dfs(r,c):
@@ -26,11 +25,9 @@ class Solution:
         for r in range(rows):
             for c in range(cols):
                 if(grid[r][c] == 1):
-                    island+=1
                     dfs(r,c)
                     max_area = max(max_area, tmp)
                     tmp = 0
 
-        print(island)
         return max_area
         
