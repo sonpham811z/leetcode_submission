b481cad18b59ecc3a55e203e4b5dbef4f12d36f0
Son Pham <142706433+sonpham811z@users.noreply.github.com>
2026-09-02 21:48:16 +0700

    Time: 5 ms (3.84%) | Memory: 21 MB (23.07%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        if not root:
+            return res
+
+        if(root.left != None):
+            leave_left = self.inorderTraversal(root.left)
+            res.extend(leave_left)
+
+        res.append(root.val)
+
+        if(root.right != None):
+            leave_right = self.inorderTraversal(root.right)
+            res.extend(leave_right)
+
+        return res 
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        res = self.inorderTraversal(root)
+
+        for i in range(len(res)-1):
+            if(res[i] >= res[i+1]):
+                return False
+            
+        return True
