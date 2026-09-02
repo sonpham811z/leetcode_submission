1702d99563a393149ae8ad5405ae40147fa3d6d7
Son Pham <142706433+sonpham811z@users.noreply.github.com>
2026-09-02 23:14:51 +0700

    Time: 0 ms (100.00%) | Memory: 20.9 MB (46.45%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -5,25 +5,39 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-
+    def validate(self, root, min_value, max_value):
         if not root:
-            return res
-
-        if(root.left != None):
-            leave_left = self.inorderTraversal(root.left)
-            res.extend(leave_left)
-
-        res.append(root.val)
-
-        if(root.right != None):
-            leave_right = self.inorderTraversal(root.right)
-            res.extend(leave_right)
-
-        return res 
-
+            return True
+        if root.val <= min_value or root.val >= max_value:
+            return False
+        
+        return self.validate(root.left, min_value, root.val) and self.validate(root.right, root.val, max_value)
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        max_value = 10000000
+        min_value = -10000000
+
+        return self.validate(root, float('-inf'), float('inf'))        
+
+
+    # def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    #     res = []
+
+    #     if not root:
+    #         return res
+
+    #     if(root.left != None):
+    #         leave_left = self.inorderTraversal(root.left)
+    #         res.extend(leave_left)
+
+    #     res.append(root.val)
+
+    #     if(root.right != None):
+    #         leave_right = self.inorderTraversal(root.right)
+    #         res.extend(leave_right)
+
+    #     return res 
+
+    # def isValidBST(self, root: Optional[TreeNode]) -> bool:
         res = self.inorderTraversal(root)
 
         for i in range(len(res)-1):
