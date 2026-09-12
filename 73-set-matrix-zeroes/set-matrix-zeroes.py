64a8b932087aca82efe932033f8d9340f196393c
Son Pham <142706433+sonpham811z@users.noreply.github.com>
2026-09-12 23:26:37 +0700

    Time: 3 ms (90.58%) | Memory: 20.8 MB (35.30%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -11,12 +11,9 @@ class Solution:
                 if matrix[i][j] == 0:
                     row_set.add(i)
                     col_set.add(j)
-        print(row_set)
-        print(col_set)
+
         for i in range(row):
             for j in range(col):
-                print(i,j,"concac")
                 if i in row_set or j in col_set:
-                    print(i,j)
                     matrix[i][j] = 0
         return matrix
